24b0df3a21c80166ee8e7b616a6d7c238645e918
Maria Gabriela <gabrielaanselmao@gmail.com>
2024-08-30 10:22:04 -0300

    desafio do exercicio05 da aula02

--- aula02/App.js
+++ aula02/App.js
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import Exercicio04 from './src/components/Exercicio04'
+import Exercicio05 from './src/components/Exercicio05'
 
 const App = () => {
   return (
     <View>
-      <Exercicio04/>
+      <Exercicio05/>
     </View>
   )
 }
