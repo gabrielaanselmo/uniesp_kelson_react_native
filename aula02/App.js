fc458ec1feb308d3a585bbc74a4331d21169ec2b
Maria Gabriela <gabrielaanselmao@gmail.com>
2024-08-29 21:17:39 -0300

    exercicios da aula02

---
+++ aula02/App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Exercicio04 from './src/components/Exercicio04'
+
+const App = () => {
+  return (
+    <View>
+      <Exercicio04/>
+    </View>
+  )
+}
+
+export default App
